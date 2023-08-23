13e6255ebd857968471b0035a63efa1376fc12b7
IgbeAjaga <ajagaigbe144@gmail.com>
2023-08-23 22:21:02 +0000

    debug user file

--- models/user.py
+++ models/user.py
@@ -1,14 +1,29 @@
 #!/usr/bin/python3
 """user class, subclass of BaseModel
 """
-
+from models.base_model import Base
 from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
-    '''representation of a customer'''
+class User(BaseModel, Base):
+    """Representation of customer(user) for a MySQL database.
 
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store users.
+        email: (sqlalchemy String): The user's email address.
+        password (sqlalchemy String): The user's password.
+        first_name (sqlalchemy String): The user's first name.
+	reviews (sqlalchemy relationship): The User-Review relationship.
+        last_name (sqlalchemy String): The user's last name.
+        places (sqlalchemy relationship): The User-Place relationship.
+    """
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
