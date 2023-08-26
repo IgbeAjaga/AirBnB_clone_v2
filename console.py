04d4bbb91a34b060216021d3acdb8d34bd2017e0
IgbeAjaga <ajagaigbe144@gmail.com>
2023-08-26 21:51:16 +0000

    update

--- console.py
+++ console.py
@@ -14,8 +14,7 @@ from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
-    """Defines the HolbertonBnB command interpreter.
-    """
+    """Defines the HolbertonBnB command interpreter."""
 
     prompt = "(hbnb) "
     __classes = {
@@ -29,16 +28,53 @@ class HBNBCommand(cmd.Cmd):
     }
 
     def emptyline(self):
-        """skips/ignores all empty spaces.
-        """
+        """Ignore empty spaces."""
         pass
 
     def do_quit(self, line):
-        """Quit command to exit the program.
-        """
+        """Quit command to exit the program."""
         return True
 
-    # ... (other methods)
+    def do_EOF(self, line):
+        """EOF signal to exit the program."""
+        print("")
+        return True
+
+    def do_create(self, line):
+        """Usage: create <class> <key 1>=<value 2> <key 2>=<value 2> ...
+        Create a new class instance with given keys/values and print its id.
+        """
+        try:
+            if not line:
+                raise SyntaxError()
+            my_list = line.split(" ")
+
+            kwargs = {}
+            for i in range(1, len(my_list)):
+                key, value = tuple(my_list[i].split("="))
+                if value[0] == '"':
+                    value = value.strip('"').replace("_", " ")
+                else:
+                    try:
+                        value = eval(value)
+                    except (SyntaxError, NameError):
+                        continue
+                kwargs[key] = value
+
+            if kwargs == {}:
+                obj = eval(my_list[0])()
+            else:
+                obj = eval(my_list[0])(**kwargs)
+                storage.new(obj)
+            print(obj.id)
+            obj.save()
+
+        except SyntaxError:
+            print("** class name missing **")
+        except NameError:
+            print("** class doesn't exist **")
+
+    # ... (rest of the code)
 
     def default(self, line):
         """retrieve all instances of a class and
