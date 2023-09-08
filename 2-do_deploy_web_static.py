f65e2565356c24d7ab482587e609d536e4878154
IgbeAjaga <ajagaigbe144@gmail.com>
2023-09-08 02:47:31 +0000

    update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,68 +1,46 @@
 #!/usr/bin/python3
 '''
-fabric script to distribute an archive to web servers
-----NEEDS TO REVISIT SCRIPT
+Fabric script to distribute an archive to web servers and deploy it.
 '''
 
 import os
 from datetime import datetime
-from fabric.api import env, local, put, run, runs_once
-
+from fabric.api import env, put, run
 
 env.hosts = ['34.138.32.248', '3.226.74.205']
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to a web server.
+    """Distributes an archive to a web server and deploys the static files.
+    
     Args:
         archive_path (str): The path of the archive to distribute.
+        
     Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
-    """
-    if not os.path.isdir("versions"):
-        os.mkdir("versions")
-    cur_time = datetime.now()
-    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        cur_time.year,
-        cur_time.month,
-        cur_time.day,
-        cur_time.hour,
-        cur_time.minute,
-        cur_time.second
-    )
-    try:
-        print("Packing web_static to {}".format(output))
-        local("tar -cvzf {} web_static".format(output))
-        archize_size = os.stat(output).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
-    except Exception:
-        output = None
-    return output
-
-
-def do_deploy(archive_path):
-    """Deploys the static files to the host servers.
-    Args:
-        archive_path (str): The path to the archived static files.
+        True if all operations have been done correctly; otherwise, False.
     """
     if not os.path.exists(archive_path):
         return False
-    file_name = os.path.basename(archive_path)
-    folder_name = file_name.replace(".tgz", "")
-    folder_path = "/data/web_static/releases/{}/".format(folder_name)
-    success = False
+
     try:
-        put(archive_path, "/tmp/{}".format(file_name))
+        # Upload the archive to the /tmp/ directory of the web server
+        put(archive_path, "/tmp/")
+        
+        # Extract the archive to the /data/web_static/releases/ directory
+        file_name = os.path.basename(archive_path)
+        folder_name = file_name.replace(".tgz", "")
+        folder_path = "/data/web_static/releases/{}/".format(folder_name)
+        
         run("mkdir -p {}".format(folder_path))
         run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
-        run("rm -rf /tmp/{}".format(file_name))
+        run("rm /tmp/{}".format(file_name))
+        
+        # Move files and create symbolic links
         run("mv {}web_static/* {}".format(folder_path, folder_path))
         run("rm -rf {}web_static".format(folder_path))
         run("rm -rf /data/web_static/current")
         run("ln -s {} /data/web_static/current".format(folder_path))
-        print('New version deployed!')
-        success = True
+        
+        return True
     except Exception:
-        success = False
-    return success
+        return False
